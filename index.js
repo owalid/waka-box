b2dbcd6cdcca137e867c8853bacee1292fd53282
owalid <othmane.el.ayadi@gmail.com>
2020-11-26 22:40:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ const bar_styles = [
   "⚪⚫"
 ];
 
-function unicodeProgressBar(p, style = 0, min_size = 20, max_size = 20) {
+function unicodeProgressBar(p, style = 7, min_size = 20, max_size = 20) {
   let d;
   let full;
   let m;
